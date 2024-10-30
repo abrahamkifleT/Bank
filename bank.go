0f7755e7b9ef2fdeecada00a8f8a1561402ee6df
abrahamkifleT <abrahamkifle1212@gmail.com>
2024-10-30 11:09:19 +0300

    Splitting code Across File In The Same Package

--- bank.go
+++ bank.go
@@ -35,15 +35,12 @@ func main() {
 		fmt.Println("ERROR")
 		fmt.Println(err)
 		fmt.Println("--------")
-		panic("can't continue, sorry.")
+		//panic("can't continue, sorry.")
 	}
 
 	for {
-		fmt.Println("What do you want to do?")
-		fmt.Println("1. Check balance")
-		fmt.Println("2. Deposit money")
-		fmt.Println("3. Withdraw money")
-		fmt.Println("4. Exit")
+
+		presentOptions()
 
 		var choice int
 		fmt.Print("Your Choice: ")
