9e69ecc64326c4405ef50119f81916a2f1de884b
abrahamkifleT <abrahamkifle1212@gmail.com>
2024-10-30 09:21:41 +0300

    handling errors

--- bank.go
+++ bank.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -8,11 +9,18 @@ import (
 
 const accountBlanceFile = "balance.txt"
 
-func getBalanceFromFile() float64 {
-	data, _ := os.ReadFile(accountBlanceFile)
+func getBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile(accountBlanceFile)
+
+	if err != nil {
+		return 1000, errors.New("failed to find balance file")
+	}
 	balanceText := string(data)
-	balance, _ := strconv.ParseFloat(balanceText, 64)
-	return balance
+	balance, err := strconv.ParseFloat(balanceText, 64)
+	if err != nil {
+		return 1000, errors.New("failed to parse stored balance value")
+	}
+	return balance, nil
 }
 func writeBalanceToFile(balance float64) {
 	blanceText := fmt.Sprint(balance)
@@ -22,7 +30,12 @@ func writeBalanceToFile(balance float64) {
 func main() {
 
 	fmt.Println("Welcome to Go Bank!")
-	var accountBalance = getBalanceFromFile()
+	var accountBalance, err = getBalanceFromFile()
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		fmt.Println("--------")
+	}
 
 	for {
 		fmt.Println("What do you want to do?")
